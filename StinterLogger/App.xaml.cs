384b09c370c256ea2dbea8a4c99274db63e0a17d
Abagofair <anderskoch.airbag@gmail.com>
2019-01-30 00:40:03 +0100

    ninject test code

--- StinterLogger/App.xaml.cs
+++ StinterLogger/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,10 +9,46 @@ using System.Windows;
 
 namespace StinterLogger
 {
+    public interface ITest
+    {
+        string OutputString();
+    }
+
+    public class Test : ITest
+    {
+        public string OutputString()
+        {
+            return "asdasdasd";
+        }
+    }
+
+    public class Mobule : Ninject.Modules.NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ITest>().To<Test>().InSingletonScope();
+            Bind<Window>().To<MainWindow>().InTransientScope();
+        }
+    }
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private IKernel _kernel;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            _kernel = new StandardKernel();
+            _kernel.Load(new Mobule());
+
+            var test = new Ninject.Parameters.ConstructorArgument("test", new Test());
+            var mw = _kernel.Get<MainWindow>(test);
+            mw.Show();
+            //Current.MainWindow.Show();
+        }
     }
 }
--- StinterLogger/MainWindow.xaml.cs
+++ StinterLogger/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,13 @@ namespace StinterLogger
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        [Inject]
+        public MainWindow(ITest test)
+        {
+            InitializeComponent();
+            InTank.Text = test.OutputString();
         }
     }
 }
