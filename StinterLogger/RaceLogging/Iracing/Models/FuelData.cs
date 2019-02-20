cc3bbb1d9dbea36e9decc126d882d60acfb20e10
Abagofair <anderskoch.airbag@gmail.com>
2019-02-20 20:43:35 +0100

    Added new fuel class separate from the observable model used in the wpf

---
+++ StinterLogger/RaceLogging/Iracing/Models/FuelData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StinterLogger.RaceLogging.Iracing.Models
+{
+    public class FuelData
+    {
+        public float FuelInTank { get; set; }
+
+        public float TotalFuelUsed { get; set; }
+
+        public float TotalRaceTime { get; set; }
+
+        public float FuelUsagePerLap { get; set; }
+
+        public float FuelToFinish { get; set; }
+
+        public float RemainingRacetime { get; set; }
+
+        public int LapsCompleted { get; set; }
+
+        public int LapsRemaining { get; set; }
+
+        public bool Units { get; set; }
+    }
+}
