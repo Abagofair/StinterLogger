7b4dea179a708ab0e4b5a1f764665d9b03d0e522
Abagofair <anderskoch.airbag@gmail.com>
2019-03-05 17:41:47 +0100

    namespace

---
+++ StinterLogger/RaceLogging/Iracing/Program/Config/ProgramConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StinterLogger.RaceLogging.Iracing.Program.Config
+{
+    public class ProgramConfig
+    {
+        public ProgramConfig()
+        {
+            this.Data = new Data();
+        }
+
+        public string Name { get; set; }
+
+        public float TelemetryUpdateFrequency { get; set; }
+
+        public int StintCount { get; set; }
+
+        public int LapsPerStint { get; set; }
+
+        public Data Data { get; set; }
+    }
+}
